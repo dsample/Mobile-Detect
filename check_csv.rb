6cf51ef366faf923991b25ad366a50213784e11a
Duncan Sample <code@antimatters.co.uk>
2013-02-22 00:22:38 +0000

    Tidied up some of the CSV processor

--- check_csv.rb
+++ check_csv.rb
@@ -3,6 +3,7 @@ require 'csv'
 require_relative 'mobile_detect'
 
 usage_data_file = "usage_data.csv"
+output_file = "updated_" + usage_data_file
 ua_column = 1
 
 add_columns = true
@@ -10,15 +11,9 @@ add_columns = true
 detect = Detect.new
 detect.load_json
 
-#ua = "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10"
-#puts detect.device_type ua
-#ua = "Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19"
-#puts detect.device_type ua
-
 csvfile = CSV.open(usage_data_file)
-#csv_out = CSV::Writer.generate(File.open('updated_' + usage_data_file, 'wb'))
 
-CSV.open('updated_' + usage_data_file, 'wb') do |csv_out|
+CSV.open(output_file, 'wb') do |csv_out|
 	csvfile.each do |row|
 		device = detect.device_type row[ua_column]
 		
@@ -29,6 +24,6 @@ CSV.open('updated_' + usage_data_file, 'wb') do |csv_out|
 		
 		csv_out << row
 	end
-	
 end
-#csv_out.close
+
+csvfile.close
