03ecdfa69720d59107c279df44ae610ece606b6b
Duncan Sample <code@antimatters.co.uk>
2013-02-24 16:49:16 +0000

    Fixed the output file definition so it gets the path correct

--- check_csv.rb
+++ check_csv.rb
@@ -3,10 +3,14 @@ require 'csv'
 require_relative 'mobile_detect'
 
 usage_data_file = "usage_data.csv"
-output_file = "updated_" + usage_data_file
 ua_column = 1
+output_file = File.dirname(usage_data_file) + "/updated_" + File.basename(usage_data_file)
 
 add_columns = true
+
+usage_data_file = ARGV[0] if ARGV[0] && ARGV[0] != '--'
+ua_column = ARGV[1] if ARGV[1] && ARGV[1] != '--'
+output_file = ARGV[2] if ARGV[2] && ARGV[2] != '--'
 
 detect = Detect.new
 detect.load_json
